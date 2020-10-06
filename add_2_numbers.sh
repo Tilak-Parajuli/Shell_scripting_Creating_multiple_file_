f4e016d09c1b931e2112fdf337980281ef1522ba
Tilak Parajuli <60753682+Tilak-Parajuli@users.noreply.github.com>
2020-10-06 09:58:49 +0545

    Add files via upload
    this script will add two numbers .
    simply type :  bash (file name).sh   //to run from terminal :)

---
+++ add_2_numbers.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Dev Tilak Parajuli "
+echo "What is your name"
+read name
+echo "Enter first number"
+read x
+echo "Enter second number"
+read y
+(( sum=x+y ))
+echo "Hello $name,  The sum is=$sum"
